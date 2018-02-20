ddc535bd510603ecbee7f06a0ffba61cb5acf094
William Born <william.born@usma.edu>
2018-02-20 14:23:43 -0500

    Initial commit. Added drive.py and image_process.py

---
+++ src/drive.py
@@ -0,0 +1,97 @@
+import rospy
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Joy
+
+buttons = [0,0,0,0,0,0,0,0,0,0,0]
+
+data = []
+
+
+def callback(data):
+    global buttons
+    buttons = data.buttons
+
+class GoForward():
+
+    
+    def __init__(self):
+        # initiliaze
+        rospy.init_node('GoForward', anonymous=False)
+
+        
+        # tell user how to stop TurtleBot
+        rospy.loginfo("To stop TurtleBot CTRL + C")
+
+        # What function to call when you ctrl + c    
+        rospy.on_shutdown(self.shutdown)
+        
+        # Create a publisher which can "talk" to TurtleBot and tell it to move
+        # Tip: You may need to change cmd_vel_mux/input/navi to /cmd_vel if you're not using TurtleBot2
+        self.cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
+
+        
+     
+
+        #TurtleBot will stop if we don't keep telling it to move.  How often should we tell it to move? 10 HZ
+        r = rospy.Rate(10);
+
+        # Twist is a datatype for velocity
+        move_cmd = Twist()
+        
+        
+        # as long as you haven't ctrl + c keeping doing...
+        while not rospy.is_shutdown():
+            # let's go forward at 0.2 m/s
+            # subscribe to joy nod
+            rospy.Subscriber("joy", Joy, callback)            
+            rospy.loginfo(buttons[0])            
+            if buttons[0] == 1:
+                data.append('x')
+                rospy.loginfo("Stop")
+                move_cmd.linear.x = 0
+                # let's turn at 0 radians/s
+                move_cmd.angular.z = 0
+            elif buttons[1] == 1:
+                data.append('r')
+                rospy.loginfo("right")
+                move_cmd.linear.x = 0.4
+                # let's turn at 0 radians/s
+                move_cmd.angular.z = -1
+            elif buttons[2] == 1:
+                data.append('l')                
+                rospy.loginfo("left")
+                move_cmd.linear.x = 0.4
+                # let's turn at 0 radians/s
+                move_cmd.angular.z = 1     
+            else:
+                data.append('s')
+                rospy.loginfo("Straight")
+                move_cmd.linear.x = 0.4
+                # let's turn at 0 radians/s
+                move_cmd.angular.z = 0
+            if buttons[3] == 1:
+                 self.shutdown();
+            # publish the velocity
+            self.cmd_vel.publish(move_cmd)
+            
+            # wait for 0.1 seconds (10 HZ) and publish again
+            r.sleep()
+    
+               
+        
+    def shutdown(self):
+        print(data)
+        # stop turtlebot
+        rospy.loginfo("Stop TurtleBot")
+        # a default Twist has linear.x of 0 and angular.z of 0.  So it'll stop TurtleBot
+        self.cmd_vel.publish(Twist())
+        # sleep just makes sure TurtleBot receives the stop command prior to shutting down the script
+        rospy.sleep(1)
+ 
+if __name__ == '__main__':
+    #try:
+    GoForward()
+    #except:
+        #rospy.loginfo("GoForward node terminated.")
+
+
---
+++ src/image_process.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import roslib
+import sys
+import rospy
+import cv2
+import numpy as np
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+class image_converter:
+
+  def __init__(self):
+    self.image_pub = rospy.Publisher("/output/image_raw",Image)
+
+    self.bridge = CvBridge()
+    self.image_sub = rospy.Subscriber("/usb_cam/image_raw",Image,self.callback)
+
+  def callback(self,data):
+    try:
+      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError as e:
+      print(e)
+
+    resized_image = cv2.resize(cv_image, (100, 100)) 
+
+    clahe = cv2.createCLAHE(clipLimit=3., tileGridSize=(8,8))
+
+    lab = cv2.cvtColor(resized_image, cv2.COLOR_BGR2LAB)  # convert from BGR to LAB color space
+    l, a, b = cv2.split(lab)  # split on 3 different channels
+
+    l2 = clahe.apply(l)  # apply CLAHE to the L-channel
+
+    lab = cv2.merge((l2,a,b))  # merge channels
+    img2 = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
+
+    gray_image = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    gray_image = cv2.cvtColor(img2, cv2.COLOR_GRAY2BGR)
+    
+    cv2.namedWindow('image')
+    showimg = cv2.resize(gray_image, (500, 500)) 
+    cv2.resizeWindow('image', 600,600)
+    cv2.imshow('image', showimg)
+    
+    cv2.waitKey(3)
+
+    try:
+      self.image_pub.publish(self.bridge.cv2_to_imgmsg(gray_image, "mono8"))
+      cv2.imwrite( "img.png", gray_image );
+    except CvBridgeError as e:
+      print(e)
+
+def main(args):  
+  rospy.init_node('image_converter', anonymous=True)
+  ic = image_converter()
+  
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    print("Shutting down")
+  cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main(sys.argv)
