2cd0d9d812283755dd7015fc3344d60c72378c36
William Born <wkb1997@me.com>
2018-04-04 14:29:14 -0400

    added plots to train.py

--- src/train.py
+++ src/train.py
@@ -13,6 +13,22 @@ from keras.layers.convolutional import *
 from matplotlib import pyplot as plt
 from sklearn.metrics import confusion_matrix
 import itertools
+import matplotlib.pyplot as plt
+
+#plots images w/ labels
+def plots(ims, figsize=(12,6), rows=1, interp=False, titles=None)
+    if type(ims[0]) is np.ndarray:
+        ims = np.array(ims).astype(np.uint8)
+        if (ims.shape[-1] !=3):
+                ims = ims.transpose((0,2,3,1))
+    f = plt.figure(figsize=figsize)
+    cols = len(ims)//rows if len(ims) % 2 == 0 else len(ims)//rows +1
+    for i in range(len(ims)):
+        sp = f.add_subplot(rows,cols,i+1)
+        sp.axis('Off')
+        if titles is not None:
+            sp.set_title(titles[i],fontsize=16)
+        plt.imshow(ims[i],interpolation=None if interp else 'none')
 
 train_path = 'set/train'
 valid_path = 'set/valid'
@@ -23,6 +39,8 @@ valid_batches = ImageDataGenerator().flow_from_directory(valid_path,target_size=
 test_batches = ImageDataGenerator().flow_from_directory(test_path,target_size=(100,100), classes=['s','l','r'], batch_size=50)
 
 imgs,labels=next(train_batches)
+
+plot(imgs, titles=labels)
 
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3, 3),
