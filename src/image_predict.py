beb52c9535942ff191acdaae21321430534fd587
William Born <william.born@usma.edu>
2018-04-05 15:24:02 -0400

    created image predictor

---
+++ src/image_predict.py
@@ -0,0 +1,44 @@
+#!/home/rrc/anaconda3/bin/python3.6
+import numpy as np
+import keras
+from keras import backend as K
+from keras.models import Sequential
+from keras.layers import Activation
+from keras.layers.core import Dense, Flatten, Dropout
+from keras.optimizers import Adam
+from keras.metrics import categorical_crossentropy
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import *
+from matplotlib import pyplot as plt
+from sklearn.metrics import confusion_matrix
+import itertools
+import matplotlib.pyplot as plt
+
+#plots images w/ labels
+def plots(ims, figsize=(12,6), rows=1, interp=False, titles=None):
+    if type(ims[0]) is np.ndarray:
+        ims = np.array(ims).astype(np.uint8)
+        if (ims.shape[-1] !=3):
+                ims = ims.transpose((0,2,3,1))
+    f = plt.figure(figsize=figsize)
+    cols = len(ims)//rows if len(ims) % 2 == 0 else len(ims)//rows +1
+    for i in range(len(ims)):
+        sp = f.add_subplot(rows,cols,i+1)
+        sp.axis('Off')
+        if titles is not None:
+            sp.set_title(titles[i],fontsize=16)
+        plt.imshow(ims[i],interpolation=None if interp else 'none')
+
+test_batches = ImageDataGenerator().flow_from_directory(test_path,target_size=(224,224), classes=['l','s','r'], batch_size=5)
+
+imgs,labels=next(train_batches)
+
+plots(imgs, titles=labels)
+plt.show()
+
+model = load_model("latest.h5")
+predictions= model.predict_generator(test_batches,steps=1,verbose=0)
+
+plots(imgs, titles=predictions)
+plt.show()
