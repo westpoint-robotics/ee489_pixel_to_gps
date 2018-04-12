0415904ded199aefc88ecdeac4440373ec6425fb
William Born <william.born@usma.edu>
2018-04-12 12:55:03 -0400

    added image_publish.py

---
+++ src/image_publish.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+from __future__ import print_function
+
+import roslib
+import sys
+import rospy
+import cv2
+import numpy as np
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+
+class image_converter:
+
+  def __init__(self):
+    self.image_pub = rospy.Publisher("/output/image_raw",Image)
+    self.bridge = CvBridge()
+    self.image_sub = rospy.Subscriber("/usb_cam/image_raw",Image,self.callback)
+    print("done.")
+
+  def callback(self,data):
+    try:
+      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError as e:
+      print(e)
+
+    resized_image = cv2.resize(cv_image, (100, 100))
+
+    cv2.waitKey(3)
+    #rospy.loginfo(current)
+    try:
+      if str(current)[7] != 'x':
+        self.image_pub.publish(self.bridge.cv2_to_imgmsg(img3, "bgr8"))
+
+    except CvBridgeError as e:
+      print(e)
+
+def main(args):
+  rospy.init_node('image_converter', anonymous=True)
+  ic = image_converter()
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    print("Shutting down")
+
+if __name__ == '__main__':
+    main(sys.argv)
